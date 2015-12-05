ef2f442de613e70ea0c30af9d5d7dbed0b31d7f2
walkerdorris <walker.dorris@gmail.com>
2015-12-05 11:27:20 -0600

    basic setup for app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var app = angular.module("Basketball-Capstone-App", ['firebase', 'ngRoute', 'ui.bootstrap']);
+
+  app.config(['$routeProvider',
+  	function($routeProvider){
+  		$routeProvider
+  		.when('/', {
+  			templateUrl:
+  			controller:
+  		})
+  	}]);
