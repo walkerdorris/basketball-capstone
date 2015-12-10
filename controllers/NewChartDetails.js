5150481b23deb1b414fdeff9ee77b9d35a3c6fd9
walkerdorris <walker.dorris@gmail.com>
2015-12-09 20:53:50 -0600

    user able to save new chart to firebase

--- controllers/NewChartDetails.js
+++ controllers/NewChartDetails.js
@@ -1,3 +1,30 @@
-app.controller("NewChartDetails", function($scope){
-	console.log("NewChartDetails");
-});
+app.controller("NewChartDetails", 
+	["Auth","$scope","$firebaseArray","$location", function(Auth,$scope, $firebaseArray, $location){
+
+	$scope.userAuth = Auth.$getAuth();
+
+	var ref = new Firebase("https://basketball-capstone.firebaseio.com/"+$scope.userAuth.uid+"/Charts/");
+
+	console.log("$scope.userAuth", $scope.userAuth);
+
+	$scope.charts = $firebaseArray(ref);
+	var gameboard = 
+	[["","",""],
+	 ["","",""],
+	 ["","",""]
+	]
+
+	$scope.newChartDetails = {};
+
+	$scope.addChart = function(){
+		console.log("hi");
+		$scope.charts.$add(
+		{
+			title: $scope.newChartDetails.title,
+			date: $scope.newChartDetails.date,
+			gameboard: gameboard
+		})
+		$location.path("/MainPage")
+	};
+
+	}]);
