bca71d53933bceef868e41774e5960dbee68f1c2
walkerdorris <walker.dorris@gmail.com>
2015-12-12 10:43:56 -0600

    retriving title and date from fb

--- controllers/OldOrNewChart.js
+++ controllers/OldOrNewChart.js
@@ -1,3 +1,13 @@
-app.controller("OldOrNewChart", function($scope){
-	console.log("OldNewChart");
-});
+app.controller("OldOrNewChart", ["Auth", "$scope", "$firebaseArray", "$location",
+	function(Auth, $scope, $firebaseArray, $location){
+	
+	$scope.userAuth = Auth.$getAuth();
+
+	var ref = new Firebase("https://basketball-capstone.firebaseio.com/"+$scope.userAuth.uid+"/Charts/");
+
+	console.log("$scope.userAuth", $scope.userAuth);
+
+	$scope.savedcharts = $firebaseArray(ref);
+
+	
+}]);
