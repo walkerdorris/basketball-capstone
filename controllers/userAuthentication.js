5c981d3a57ca885749c569450a632721a4a220e7
walkerdorris <walker.dorris@gmail.com>
2015-12-31 18:37:00 -0600

    basic styling complete

--- controllers/userAuthentication.js
+++ controllers/userAuthentication.js
@@ -37,9 +37,39 @@ app.controller("AuthCtrl",
 	 		},function(error){
 	 			console.log(error);
 	 		})
-	 	// 	.bind(this).catch(function(error){
-			// 	console.log("Authentication Failed: ", error);
-			// });
+	 	
 	 	};
 	 }
+
+
+
+
+// *************************************
+// *************JQUERY FOR CSS**********
+// *************************************
+
+
+
+
+    
+
+
+
+
 	]);
+
+
+// $('#login-form-link').click(function(e) {
+// 		$("#login-form").delay(100).fadeIn(100);
+//  		$("#register-form").fadeOut(100);
+// 		$('#register-form-link').removeClass('active');
+// 		$(this).addClass('active');
+// 		e.preventDefault();
+// 	});
+// 	$('#register-form-link').click(function(e) {
+// 		$("#register-form").delay(100).fadeIn(100);
+//  		$("#login-form").fadeOut(100);
+// 		$('#login-form-link').removeClass('active');
+// 		$(this).addClass('active');
+// 		e.preventDefault();
+// 	});
