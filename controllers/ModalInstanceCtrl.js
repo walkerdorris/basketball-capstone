2844f6a8986d3fac858b185b2db68028ceb3dc23
walkerdorris <walker.dorris@gmail.com>
2016-01-02 17:39:42 -0600

    mvp

--- controllers/ModalInstanceCtrl.js
+++ controllers/ModalInstanceCtrl.js
@@ -1,50 +1,50 @@
 app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, myCellId, gameBoard, updateFirebase, makeCounter, missCounter, totalShots) {
-  console.log("Beginning of ModalInstance.js");
-
   $scope.cellId = myCellId; 
   $scope.gameBoard = gameBoard;
-  console.log("$scope.gameBoard", $scope.gameBoard);
   $scope.updateFb = updateFirebase;
 
   $scope.makeCounter = makeCounter;
-  console.log("modal $scope.makeCounter", $scope.makeCounter);
   $scope.missCounter = missCounter;
   $scope.totalShots = totalShots;
 
   // ****************************
   // ********MAKES W/ MODAL******
   // ****************************
-
    $scope.make = function() {
       $scope.gameBoard[$scope.cellId] = 'make';
       $scope.makeCounter++;
-      console.log("make function in modal", $scope.makeCounter);
       $scope.updateFb.update({gameBoard: $scope.gameBoard});
-      $modalInstance.close({gameBoard:$scope.gameBoard});
+      $scope.totalShots = $scope.makeCounter+$scope.missCounter;
+      $modalInstance.close({gameBoard:$scope.gameBoard, makeCounter:$scope.makeCounter, missCounter:$scope.missCounter, totalShots:$scope.totalShots});
    };
 
    // ***************************
    // ******MISSES W/ MODAL******
    // ***************************
-   
    $scope.miss = function() {
       $scope.gameBoard[$scope.cellId] = 'miss';
+      $scope.missCounter++;
       $scope.updateFb.update({gameBoard: $scope.gameBoard});
-      $modalInstance.close({gameBoard:$scope.gameBoard});
+      $scope.totalShots = $scope.makeCounter+$scope.missCounter;
+      $modalInstance.close({gameBoard:$scope.gameBoard, makeCounter:$scope.makeCounter, missCounter:$scope.missCounter, totalShots:$scope.totalShots});
    };
 
    // ***************************
    // *******DELETE W/MODAL******
    // ***************************
-   
    $scope.deleted = function() {
       $scope.gameBoard[$scope.cellId] = '';
       $scope.updateFb.update({gameBoard: $scope.gameBoard});
-      $modalInstance.close({gameBoard:$scope.gameBoard});
+      $scope.makeCounter=0;
+      $scope.missCounter=0;
+      for (var i=0; i<$scope.gameBoard.length; i++){
+        if($scope.gameBoard[i]==='miss'){
+          $scope.missCounter++;
+        } else if($scope.gameBoard[i]==='make'){
+          $scope.makeCounter++;
+        }
+      }  
+      $scope.totalShots = $scope.makeCounter+$scope.missCounter;
+      $modalInstance.close({gameBoard:$scope.gameBoard, makeCounter:$scope.makeCounter, missCounter:$scope.missCounter, totalShots:$scope.totalShots});
    };
-
-     
-
-     console.log("End of ModalInstance.js");
-
 });
