5a57c27e135a1d8483754246a3714176cf8a3a58
walkerdorris <walker.dorris@gmail.com>
2016-01-02 11:21:51 -0600

    old gameboards can be used

--- controllers/OldOrNewChart.js
+++ controllers/OldOrNewChart.js
@@ -1,5 +1,23 @@
-app.controller("OldOrNewChart", ["Auth", "$scope", "$firebaseArray", "$location",
-	function(Auth, $scope, $firebaseArray, $location){
+// app.controller("OldOrNewChart", ["Auth", "$scope", "$firebaseArray", "$location",
+// 	function(Auth, $scope, $firebaseArray, $location){
+	
+// 	$scope.userAuth = Auth.$getAuth();
+
+// 	var ref = new Firebase("https://basketball-capstone.firebaseio.com/"+$scope.userAuth.uid+"/Charts/");
+
+// 	console.log("$scope.userAuth", $scope.userAuth);
+
+// 	$scope.savedcharts = $firebaseArray(ref);
+
+	
+// }]);
+
+
+
+
+
+app.controller("OldOrNewChart", ["Auth", "$scope", "$firebaseArray", "$location", "$routeParams",
+	function(Auth, $scope, $firebaseArray, $location, $routeParams){
 	
 	$scope.userAuth = Auth.$getAuth();
 
@@ -9,5 +27,15 @@ app.controller("OldOrNewChart", ["Auth", "$scope", "$firebaseArray", "$location"
 
 	$scope.savedcharts = $firebaseArray(ref);
 
+
+
+	$scope.loadoldchart = function(id){
+
+		console.log("id", id);
+		$location.path("/MainPage/"+id);
+	};
 	
+
+
+
 }]);
